eb00a618150ee9f36ed17e653584e05557d519d0
Ahab Fadl <ehabahmedfadl@gmail.com>
2023-11-13 02:43:31 +0300

    first

---
+++ Problem48.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome, in this program I will calculate how much you will pay at month!\n";
+
+    float LoanAmount, MonthlyPayment, Months;
+
+    cout << "\nFirst, how much the loan?\n";
+    cin >> LoanAmount;
+
+    cout << "\nHow many months do you like to settle the loan after?\n";
+    cin >> Months;
+
+    MonthlyPayment = LoanAmount / Months;
+
+    cout << "\nOkay dude, you will pay: " << MonthlyPayment << " every month to settle the loan.\n";
+
+    return 0;
+}
